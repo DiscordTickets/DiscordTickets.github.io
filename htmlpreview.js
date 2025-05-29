ce8f535d45d2d6ef24ea2e0aef686dac73e43667
DiscordTickets <nsjswnuxh+1@gmail.com>
2025-05-28 19:52:51 -0500

    Update htmlpreview.js

--- htmlpreview.js
+++ htmlpreview.js
@@ -1,52 +1,76 @@
 (function () {
-    
     var previewForm = document.getElementById('previewform');
 
-    // Get the URL from the query string.
-    // For GitHub URLs, transform them to raw.githubusercontent.com.
+    // Extract URL from the query string.
+    // For GitHub URLs, we perform a transformation to obtain the raw file URL.
     var url = location.search.substring(1)
-              .replace(/\/\/github\.com/, '//raw.githubusercontent.com')
-              .replace(/\/blob\//, '/');
+        .replace(/\/\/github\.com/, '//raw.githubusercontent.com')
+        .replace(/\/blob\//, '/');
+
+    // fetchProxy downloads the resource as a Blob and converts it to text before resolving.
+    var fetchProxy = function (url, options, i) {
+        var proxy = [
+            '', // attempt direct fetch first.
+            'https://api.codetabs.com/v1/proxy/?quest=' // fallback proxy.
+        ];
+        return fetch(proxy[i] + url, options).then(function (res) {
+            if (!res.ok)
+                throw new Error('Cannot load ' + url + ': ' + res.status + ' ' + res.statusText);
+            return res.blob();
+        }).then(function (blob) {
+            // Return the full file content as text.
+            return new Promise(function (resolve, reject) {
+                var reader = new FileReader();
+                reader.onload = function () {
+                    resolve(reader.result);
+                };
+                reader.onerror = function (err) {
+                    reject(err);
+                };
+                reader.readAsText(blob);
+            });
+        }).catch(function (error) {
+            if (i === proxy.length - 1) throw error;
+            return fetchProxy(url, options, i + 1);
+        });
+    };
 
     var replaceAssets = function () {
         var frame, a, link, links = [], script, scripts = [], i, href, src;
-        
-        // Do nothing for framesets because document.write() will erase things.
+
+        // If a frameset is present, exit because document.write() would clear it.
         if (document.querySelectorAll('frameset').length)
             return;
-        
-        // Process frames (iframe and frame elements).
+
+        // Process any <iframe> or <frame> tags.
         frame = document.querySelectorAll('iframe[src],frame[src]');
         for (i = 0; i < frame.length; ++i) {
-            src = frame[i].src; // Get absolute URL
-            // Check if the URL comes from GitHub, Bitbucket, or Discord.
+            src = frame[i].src;
             if (src.indexOf('//raw.githubusercontent.com') > 0 ||
                 src.indexOf('//bitbucket.org') > 0 ||
                 src.indexOf('discordapp.com') > 0) {
-                // Rewrite the source URL so it loads using the CORS proxy.
                 frame[i].src = '//' + location.hostname + location.pathname + '?' + src;
             }
         }
-        
-        // Process anchor links.
+
+        // Update all <a> links.
         a = document.querySelectorAll('a[href]');
         for (i = 0; i < a.length; ++i) {
             href = a[i].href;
             if (href.indexOf('#') > 0) {
-                // For anchors, rewrite to support empty anchors.
-                a[i].href = '//' + location.hostname + location.pathname + location.search + '#' + a[i].hash.substring(1);
+                a[i].href = '//' + location.hostname + location.pathname + location.search +
+                    '#' + a[i].hash.substring(1);
             } else if (
                 (href.indexOf('//raw.githubusercontent.com') > 0 ||
                  href.indexOf('//bitbucket.org') > 0 ||
                  href.indexOf('discordapp.com') > 0) &&
                 (href.indexOf('.html') > 0 || href.indexOf('.htm') > 0)
             ) {
-                // For links to other HTML pages on these hosts, rewrite URL through the proxy.
                 a[i].href = '//' + location.hostname + location.pathname + '?' + href;
             }
         }
-        
-        // Process stylesheets.
+
+        // Process external CSS files.
         link = document.querySelectorAll('link[rel=stylesheet]');
         for (i = 0; i < link.length; ++i) {
             href = link[i].href;
@@ -61,8 +85,8 @@
                 loadCSS(res[i]);
             }
         });
-        
-        // Process scripts. Scripts using type="text/htmlpreview" are loaded via proxy.
+
+        // Process external or inline scripts (temporarily marked with type="text/htmlpreview").
         script = document.querySelectorAll('script[type="text/htmlpreview"]');
         for (i = 0; i < script.length; ++i) {
             src = script[i].src;
@@ -79,17 +103,17 @@
             for (i = 0; i < res.length; ++i) {
                 loadJS(res[i]);
             }
-            // Dispatch a fake DOMContentLoaded event after scripts are loaded.
             document.dispatchEvent(new Event('DOMContentLoaded', { bubbles: true, cancelable: true }));
         });
     };
 
     var loadHTML = function (data) {
         if (data) {
-            // Inject a <base> tag in the <head> so that relative URLs work,
-            // and replace script tags to use a temporary type.
+            // Insert a <base> tag so that relative URLs resolve correctly,
+            // and temporarily change script types so they don’t execute immediately.
             data = data.replace(/<head([^>]*)>/i, '<head$1><base href="' + url + '">')
-                       .replace(/<script(\s*src=["'][^"']*["'])?(\s*type=["'](text|application)\/javascript["'])?/gi, '<script type="text/htmlpreview"$1');
+                .replace(/<script(\s*src=["'][^"']*["'])?(\s*type=["'](text|application)\/javascript["'])?/gi,
+                    '<script type="text/htmlpreview"$1');
             setTimeout(function () {
                 document.open();
                 document.write(data);
@@ -115,44 +139,16 @@
         }
     };
 
-    // Modified fetchProxy that downloads as a Blob then converts to text.
-    var fetchProxy = function (url, options, i) {
-        var proxy = [
-            '', // try fetching without a proxy first.
-            'https://api.codetabs.com/v1/proxy/?quest=' // second option using CodeTabs proxy.
-        ];
-        return fetch(proxy[i] + url, options).then(function (res) {
-            if (!res.ok)
-                throw new Error('Cannot load ' + url + ': ' + res.status + ' ' + res.statusText);
-            // Instead of calling res.text() immediately,
-            // read the content as a Blob first.
-            return res.blob();
-        }).then(function(blob) {
-            // Use FileReader to convert blob to text.
-            return new Promise(function(resolve, reject) {
-                var reader = new FileReader();
-                reader.onload = function() {
-                    resolve(reader.result);
-                };
-                reader.onerror = reject;
-                reader.readAsText(blob);
+    // Entry point: if a valid URL is provided and it’s not on our own domain, fetch it.
+    if (url && url.indexOf(location.hostname) < 0) {
+        fetchProxy(url, null, 0)
+            .then(loadHTML)
+            .catch(function (error) {
+                console.error(error);
+                previewForm.style.display = 'block';
+                previewForm.innerText = error;
             });
-        }).catch(function (error) {
-            if (i === proxy.length - 1)
-                throw error;
-            return fetchProxy(url, options, i + 1);
-        });
-    };
-
-    // If a valid URL is provided and it’s not already on our domain, fetch and load it;
-    // otherwise, show the preview form (for error messages, etc.).
-    if (url && url.indexOf(location.hostname) < 0)
-        fetchProxy(url, null, 0).then(loadHTML).catch(function (error) {
-            console.error(error);
-            previewForm.style.display = 'block';
-            previewForm.innerText = error;
-        });
-    else
+    } else {
         previewForm.style.display = 'block';
-
+    }
 })();
